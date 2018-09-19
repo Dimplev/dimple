411dca961d2559aa88ab79f5a32d3144c7ade2f3
Dimplev <43195826+Dimplev@users.noreply.github.com>
2018-09-19 10:09:04 +0530

    Add files via upload

---
+++ sampledice.py
@@ -0,0 +1,11 @@
+#write a program to print random numbers specify
+import random
+while True:
+	x=input("enter 'r' to roll a dice and 'q' to quit")
+	if(x=='r'):
+		print(random.randint(1,6))
+	elif(x=='q'):
+		print("bye!")
+		exit()
+	else:
+		print("give either 'r' or 'q'")
