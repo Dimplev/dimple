489c6fa149f49191c004b742b5d5b3e1a13b5188
Dimplev <43195826+Dimplev@users.noreply.github.com>
2018-09-26 09:39:22 +0530

    Add files via upload

---
+++ usingfunctions.py
@@ -0,0 +1,24 @@
+#using functions
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+def add():
+	c=a+b
+	return c
+	def sub():
+		d=a-b
+		return d
+		def div():
+			e=a/b
+			return e
+			def multi():
+				f=a*b
+				return f
+				res=add()
+				res1=sub()
+				res3=div()
+				res4=multi()
+				print("addition of a and b"res)
+				print("subtraction of a and b"res1)
+				print("division of a and b"res3)
+				print("multiplication of a and b"res4)
+	
