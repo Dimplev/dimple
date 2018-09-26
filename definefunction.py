8c7ef861fd147bd2641030a88e3cb1db753198e8
Dimplev <43195826+Dimplev@users.noreply.github.com>
2018-09-26 10:37:27 +0530

    Add files via upload

---
+++ definefunction.py
@@ -0,0 +1,24 @@
+i=int(input("enter the value of i: "))
+j=int(input("enter the value of j: "))
+o=input("what do u want to do? +,-,*,/: ")
+
+def add():
+	return i+jsszs
+	def sub(a,b):
+		return a-b
+		def mult(a,b):
+			return a*b
+			def divide(a,b):
+				return a/b
+
+if(o=='+'):
+	print("addition=",add())s
+elif(o=='-'):
+	print("subtraction=",sub(50,80))
+elif(o=='*'):
+	res=mul(i,j)
+	print("multiplication="res)
+elif(o=='/'):
+	print("division="div(i,j))
+
+
