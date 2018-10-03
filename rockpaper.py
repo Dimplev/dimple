2d1d2913289407086a0112885121ab8d56ebef4a
Dimplev <43195826+Dimplev@users.noreply.github.com>
2018-10-03 08:45:52 +0530

    Add files via upload

---
+++ rockpaper.py
@@ -0,0 +1,18 @@
+import random
+
+p={1:'r',2:'p',3:'s'}
+
+while True:
+	yc=input("YOU CHOOSE: r/p/s: ")
+	cc=p[random.randint(1,3)]
+
+	print("computer gave: ",cc)
+
+	if(yc=='r' and cc=='p'):
+		print("computer wins the game")
+	elif(yc=='s' and cc=='p'):
+		print("you win the game")
+	elif(yc=='r' and cc=='s'):
+		print("you win the game")
+	elif(yc=='r' and cc=='p'):
+		print("computer wins the game")
